fea46743d5f811421370782ce501bd2aeb475354
Yaron <y.lambers@outlook.com>
2015-11-20 12:33:09 +0100

    Added other structure

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yaron
+ * Date: 20-11-2015
+ * Time: 12:01
+ */
+
+class Database{
+    private $pdo;
+    public function opendb(){
+
+        $server = "localhost";
+        $user = "root";
+        $pass = "";
+        $dbName = "ticketSystem";
+
+       $this->pdo = new PDO(sprintf('mysql:host=%s;dbName=%s', $server, $dbName), $user, $pass);
+    }
+}
