248c6a94b924dad8d4504589a01e9e2d50fcddad
Yaron <y.lambers@outlook.com>
2015-12-03 22:29:17 +0100

    ticket

--- includes/ticket.php
+++ includes/ticket.php
@@ -6,7 +6,6 @@
  * Time: 18:17
  */
 require_once('database.php');
-
     if (isset($_POST['ticket'])) {
         $problem = mysqli_real_escape_string($db, $_POST['problemname']);
         $description = mysqli_real_escape_string($db, $_POST['description']);
@@ -19,9 +18,11 @@ require_once('database.php');
 
         if ($error == 0){
             $query = "INSERT INTO ticket (problemname, description, user_id) VALUES ('$problem', '$description', '$user')";
-
             if (!mysqli_query($db, $query)) {
                 die('Error ' . mysqli_error($db));
+            }else{
+                $problem = '';
+                $description = '';
             }
         }
         if(strlen($error) > 1){
@@ -35,8 +36,6 @@ require_once('database.php');
         <input type="text" name="problemname" id="problemname" value="'.$problem.'"> </input> </br>
         <label>Complete probleem beschrijving: </label> <br />
         <input type="text" name="description" id="description" value="'.$description.'"> </input> <br />
-        <label>User</label> <br/>
-        <input type="text" name="user_id" id="user_id"> </input> <br/>
         <input type="submit" name="ticket" value="toevoegen" />
         </form>
     ';
@@ -44,3 +43,4 @@ require_once('database.php');
 
 mysqli_close($db);
 
+?>
